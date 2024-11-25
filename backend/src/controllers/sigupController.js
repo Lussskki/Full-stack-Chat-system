e4f8c4f870d2cb9c70ca2238d594b928d6c68d8b
Luka Guledani <lukaguledani123@gmail.com>
2024-11-25 20:49:43 +0400

    code for register user

---
+++ backend/src/controllers/sigupController.js
@@ -0,0 +1,43 @@
+// src/controllers/sigup.controller.js
+import UserSchema from "../db/mongoDbschema/userSchema.js"
+// To hash passwords
+import bcrypt from 'bcrypt'
+//  Token
+import generateToken from '../utils/jwt.js'
+
+
+const signup = async (req,res) => {
+    const {nick, password} = req.body
+
+    if (!nick || !password) {
+        return res.status(400).json({message: 'Please provide Nick or Password'})
+    }
+    try{
+        const existingUser = await UserSchema.find({ $or: [{nick}, {password}]})
+        if (!existingUser) {
+            return res.status(400).json({message: 'Nick is already taken'})
+        }
+
+        // Hash password
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        // New user
+        const newUser = new UserSchema({nick, password: hashPassword})
+        await newUser.save() 
+
+        // Generate token
+        const token = generateToken(newUser._id, newUser.nick)
+
+        // Respond with success and the generated token
+        res.status(201).json({
+            message: 'User created successfully',
+            token,
+          })
+        // console.log(token)  
+        } catch (error) {
+          console.error('Error creating user:', error)
+          res.status(500).json({ message: 'Server error' })
+        }
+}
+
+export default signup
