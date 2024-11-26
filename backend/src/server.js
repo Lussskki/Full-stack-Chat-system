aadb71e1164456728e299d2d228dd9aaca25dfc7
Luka Guledani <lukaguledani123@gmail.com>
2024-11-26 21:04:35 +0400

    added login route

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,18 @@
 import express from 'express'
 // Database 
 import mongoDB from './db/mongoDb.js'
+// Routes
+import signUp from './routes/signupRoute.js'
+import login from './routes/loginRoute.js'
 
-import signup from './controllers/sigupController.js'
 
-const router = express.Router()
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
 
-app.use('/', signup)
+app.use('/signup', signUp)
+app.use('/login', login)
 
 app.listen(PORT, () => {
     console.log('App listens to port 3000')
