eea09669799ab4aa62ad17c27d216bf27160a683
Luka Guledani <lukaguledani123@gmail.com>
2024-11-27 17:52:40 +0400

    add profile route

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import mongoDB from './db/mongoDb.js'
 // Routes
 import signUp from './routes/signupRoute.js'
 import login from './routes/loginRoute.js'
+import profile from './routes/profileRoute.js'
 
 
 const app = express()
@@ -14,6 +15,7 @@ app.use(express.json())
 
 app.use('/signup', signUp)
 app.use('/login', login)
+app.use('/profile', profile)
 
 app.listen(PORT, () => {
     console.log('App listens to port 3000')
