8280008e0fc3d8e59212f2eda62415949a3dcb8c
Luka Guledani <lukaguledani123@gmail.com>
2024-11-27 18:30:13 +0400

    create and manage profile auth

---
+++ backend/src/utils/profileMiddleware.js
@@ -0,0 +1,21 @@
+// src/controllers/profileController.js
+import jwt from 'jsonwebtoken'
+ 
+// auth middleware for profile
+const authMiddleware = async (req, res) => {
+    const token = req.headers['authorization']?.split('')[1]
+
+    if (!token) {
+        return res.status(401).json({message: 'No token provided'})
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JwtSecret)
+        req.user = decoded
+        next()
+    }catch(error){
+        return res.status(401).json({message:'Invalid token'})
+    }
+}
+
+export default authMiddleware
